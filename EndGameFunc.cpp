977dfa6f7eb0348f2d419c017be4f42b6d2c4c81
InsaneD69 <90348162+InsaneD69@users.noreply.github.com>
2021-09-08 23:02:43 +0300

    Add files via upload

---
+++ EndGameFunc.cpp
@@ -0,0 +1,21 @@
+﻿
+
+#include <iostream>
+#include <string>
+#include <ctime>
+using namespace std;
+
+
+void EndGameFunc(string BlackorWhite) {
+	if (BlackorWhite == "white") { cout << "black win" << endl; system("pause"); exit(0); }
+	else if (BlackorWhite == "black") { cout << "white win" << endl; system("pause"); exit(0); }
+	else { cout << "Error"; system("pause"); exit(0); };
+};
+
+int main()
+{
+	string Gyt[2] = { "white","black" };
+	srand(time(NULL));
+	EndGameFunc(Gyt[rand()%2]);
+}
+
